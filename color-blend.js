e1de3b81ee078d32a5cd2bfad43c0634964cd11f
Pooja Saxena <pooja335@gmail.com>
2015-11-04 18:01:39 -0600

    boxes show up

---
+++ color-blend.js
@@ -0,0 +1,10 @@
+$(function () {
+    
+  $("#main").empty();
+  var num_squares = 6;
+  var ii;
+  for (ii = 0; ii < num_squares; ii++) {
+    $("#main").append("<div class='board-squares'></div>");
+  }
+
+});
